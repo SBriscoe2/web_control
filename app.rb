dcc106af3edbc6ac9e055122559b631436d57088
SBriscoe2 <shabris@tiscali.co.uk>
2018-11-13 10:02:32 +0000

    added form requesting name

--- app.rb
+++ app.rb
@@ -10,8 +10,13 @@ get '/secret' do
   'I know something you do not know!!!'
 end
 
-get '/cat' do
+get '/random-cat' do
   @cat_name = ["Amigo", "Oscar", "Viking"].sample
   erb(:index)
+end
 
+get '/named-cat' do
+  p params
+  @cat_name = params[:name]
+  erb(:index)
 end
